e2309ead4aff91f244c65983c55d5203e0bf4802
Mauricio Bautista <maufbl10@gmail.com>
2025-05-25 15:21:01 -0600

    corrected terraform format

--- infra/terraform/airflow_ec2.tf
+++ infra/terraform/airflow_ec2.tf
@@ -45,9 +45,9 @@ resource "aws_security_group" "airflow_sg" {
   }
 
   ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
